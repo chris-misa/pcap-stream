691ba569971bf5fde0b815a4070ceb6298eaa29b
Chris Misa <cmisa@cs.uoregon.edu>
2021-01-05 01:51:00 -0800

    Kludged fork and join by bouncing through queues

--- pcap_stream.ml
+++ pcap_stream.ml
@@ -7,14 +7,15 @@ open Streaming
 
 open Utils
 
-(* Bring filter and map into scope *)
-let filter f = Stream.filter (fun p -> Tuple.mem "epoch" p || f p)
+let init_table_size = 1000
+
+(* Make filter and map pass epoch markers *)
+let filter f = Stream.filter (fun p -> (Tuple.mem "epoch" p) || (f p))
 let map f = Stream.map (fun p ->
     match Tuple.mem "epoch" p with
         | true -> p
         | false -> f p
     )
-        
 
 (* Produce a stream of tuples parsed from headers in a given pcap file *)
 let of_pcap_file (file_name : string) : tuple stream =
@@ -38,7 +39,7 @@ let dump outc (s : tuple stream) : unit =
     s
     |> Stream.each (fun p -> dump_tuple outc p)
 
-(* Insert a tuple with "epoch" => epoch_id, "time" => current time every dur seconds *)
+(* Insert an epoch marker (i.e., a tuple with "epoch" and "time" fields) every dur seconds *)
 let epoch (dur : float) (s : tuple stream) : tuple stream =
     let e = ref 0.0 in
     let eid = ref 0 in
@@ -64,18 +65,17 @@ let epoch (dur : float) (s : tuple stream) : tuple stream =
         )
     )
 
-let init_table_size = 1000
-
 let split_on_epoch last_eid s =
     s
     |> Stream.split ~by:(fun p ->
         match Tuple.find_opt "epoch" p with
-            | Some (Int eid) -> (last_eid := eid ; true)
+            | Some _ -> (last_eid := p ; true)
             | _ -> false
     )
 
+(* Form groups as determined by k, folding each group with f whose result is stored in res_key *)
 let groupby (k : tuple -> tuple) (f : op_result -> tuple -> op_result) (res_key : string) (s : tuple stream) : tuple stream =
-    let last_eid = ref 0 in
+    let last_eid = ref Tuple.empty in
     s
     |> split_on_epoch last_eid
     |> Stream.flat_map (fun epoch ->
@@ -92,106 +92,70 @@ let groupby (k : tuple -> tuple) (f : op_result -> tuple -> op_result) (res_key 
         (Hashtbl.fold (fun k v s ->
             Stream.append (Tuple.add res_key v k) s
         ) m Stream.empty)
-        |> Stream.append (tuple_of_list [("epoch", Int !last_eid)])
+        |> Stream.append !last_eid
     )
 
-(* BROKEN *)
-let split s_in =
-    let clone s =
-        Stream.unfold s (fun s ->
-            match Stream.first s with (* the problem is this consumes s! *)
-                | Some a -> Some (a, Stream.prepend a s) (* then this just reverse the progress *)
-                | None -> None
-        )
-    in (clone s_in, clone s_in)
+(* Returns a tuple of streams with the same contents as s *)
+let fork s = 
+    let q1 = Queue.create () in
+    let q2 = Queue.create () in
+    let sink = Sink.make
+        ~init:(fun () -> ())
+        ~push:(fun () a -> (Queue.push a q1 ; Queue.push a q2 ; ()))
+        ~full:(fun () -> false)
+        ~stop:(fun () -> ())
+        ()
+    in s |> Stream.into sink;
+    (Stream.from (Source.queue q1), Stream.from (Source.queue q2))
 
-(* this can probably be simplified a bunch by
-1. Converting the two input streams to sources (use Source.unfold as below generically)
-2. using Source.zip_with 
-*)
+let interleave l r =
+    let q1 = l |> Stream.into Sink.queue in
+    let q2 = r |> Stream.into Sink.queue in
+    Stream.(
+    (Source.zip_with
+        (fun a b -> Stream.double a b)
+        (Source.queue q1)
+        (Source.queue q2)
+    |> Stream.from
+    |> Stream.flatten)
+    ++ (Source.queue q1 |> from)
+    ++ (Source.queue q2 |> from)
+    )
+
+(* Inner join of l and r. The key functions produce key, value pairs where values from bot strings are passed on the output *)
 let join (lkey : tuple -> (tuple * tuple)) (rkey : tuple -> (tuple * tuple)) l r =
     let ltbl = Hashtbl.create init_table_size in
     let rtbl = Hashtbl.create init_table_size in
-    let proc_one m m' f p_opt =
-        match p_opt with
-            | Some p -> (
-                let k, v = f p in
-                match Hashtbl.find_opt m' k with
-                    | Some v' ->
-                        let use_left = fun _ a _ -> Some a in
-                        Some (Tuple.union use_left k (Tuple.union use_left v v'))
-                    | None -> (
-                        Hashtbl.replace m k v;
-                        None
-                    )
-            )
-            | None -> None
-    in
-    (* Merge left and right strings into a stream of (left,right) tuples *)
-    Stream.from (
-        Source.unfold (l,r) (fun (l, r) ->
-            match (Stream.first l, Stream.first r) with (* Don't need Stream.rest l/r? *)
-                | (Some a, Some b) -> Some ((Some a, Some b), (l, r))
-                | (Some a, None) -> Some ((Some a, None), (l, r))
-                | (None, Some b) -> Some ((None, Some b), (l, r))
-                | (None, None) -> None
+    let proc_one m m' f p =
+        let epoch = ref 0 in
+        match Tuple.find_opt "epoch" p with
+        | Some (Int e) -> (
+            epoch := e + 1 ;
+            Some p
         )
+        | _ -> (
+            let k, v = f p in
+            let key = (Tuple.add "cur_epoch" (Int !epoch) k) in
+            match Hashtbl.find_opt m' key with
+                | Some v' -> (
+                    let ul = fun _ a _ -> Some a in
+                    Hashtbl.remove m' key;
+                    Some (Tuple.union ul k (Tuple.union ul v v'))
+                )
+                | None -> (
+                    Hashtbl.add m key v ;
+                    None
+                )
+        )
+    in
+    interleave
+        (l |> Stream.map (fun p -> (true,p)))
+        (r |> Stream.map (fun p -> (false,p)))
+    |> Stream.flat_map (fun lrp ->
+        let one_res = match lrp with
+            | (true,p) -> proc_one ltbl rtbl lkey p
+            | (false,p) -> proc_one rtbl ltbl rkey p
+        in match one_res with
+            | Some p -> Stream.single p
+            | None -> Stream.empty
     )
-    (* Apply processing for packets in both left and right streams, merging results *)
-    |> Stream.flat_map (fun (a, b) ->
-        match (proc_one ltbl rtbl lkey a, proc_one rtbl ltbl rkey b) with
-            | (Some lres, Some rres) -> Stream.double lres rres
-            | (Some lres, None) -> Stream.single lres
-            | (None, Some rres) -> Stream.single rres
-            | (None, None) -> Stream.empty
-    )
-        
-
-    
-    
-
-(*
-
-Probably need good/better utils for abstracting actions around tuples
-- field access
-- modifying field values
-- changing keys
-
-join:
-    split each stream by epoch markers
-    fold each stream,epoch into hashtbl
-    emit matching between hashtbls as result stream
-
-... can do the thing where we keep table of key,value for each input, then emit tuple on match from either stream's process ... the issue if how to merge the resulting two match streams.
-
-
-Idea 2:
-
-both streams feed into respective hash tables.
-
-
-Idea 3:
-
-Hack to get interleaving:
-
-let merge l r =
-    Source.unfold (l,r) (fun (l, r) ->
-        match (Stream.first l, Stream.first r) with
-            | (Some a, Some b) -> Some ((a,b), (Stream.rest l, Stream.rest r))
-            | _ -> None
-    )
-
-... like a classic element-wise zip
-
-let zipper l r =
-    Source.unfold (l,r) (fun (l,r) ->
-        match (Stream.first l, Stream.first r) with
-            | (Some a, _) -> Some (a, (Stream.rest l, r))
-            | (_, Some a) -> Some (a, (l, Stream.rest r))
-            | (None, None) -> None
-    )
-
-... yeilds stream with all the elements of l followed by all the elements of r
-
-*)
-
--- queries.ml
+++ queries.ml
@@ -2,7 +2,6 @@ open Printf
 open Utils
 open Pcap_stream
 
-
 (* Sonata 1 *)
 let tcp_new_cons s =
     let threshold = 1 in
@@ -14,7 +13,49 @@ let tcp_new_cons s =
     |> groupby (filter_keys ["ipv4.dst"]) count "cons"
     |> filter (fun p -> (find_int "cons" p) >= threshold)
 
-let current_query = tcp_new_cons
+(* Sonata 5 *)
+let ddos s =
+    let threshold = 2 in
+    s
+    |> epoch 1.0
+    |> groupby (filter_keys ["ipv4.src" ; "ipv4.dst"]) distinct ""
+    |> groupby (filter_keys ["ipv4.dst"]) count "srcs"
+    |> filter (fun p -> (find_int "srcs" p) >= threshold)
+
+(* Sonata 7 *)
+let completed_flows s =
+    let threshold = 1 in
+    let a, b =
+        s
+        |> filter (fun p -> (find_int "ipv4.proto" p) = 6)
+        |> epoch 5.0 (* Adjusted *)
+        |> fork in
+    let syns =
+        a
+        |> filter (fun p -> (find_int "l4.flags" p) = 2)
+        |> groupby (filter_keys ["ipv4.dst"]) count "syns" in
+    let fins =
+        b
+        |> filter (fun p -> ((find_int "l4.flags" p) land 1) = 1)
+        |> groupby (filter_keys ["ipv4.src"]) count "fins"
+    in join
+        (fun p -> (rename_keys [("ipv4.dst", "host")] p, filter_keys ["syns"] p))
+        (fun p -> (rename_keys [("ipv4.src", "host")] p, filter_keys ["fins"] p))
+        syns
+        fins
+    |> map (fun p -> Tuple.add "diff" (Int ((find_int "syns" p) - (find_int "fins" p))) p)
+    |> filter (fun p -> (find_int "diff" p) >= threshold)
+    
+let fork_join_test s =
+    let a, b = s |> epoch 1.0 |> fork in
+    let syns = a |> filter (fun p -> (find_int "l4.flags" p) = 2) in
+    let synacks = b |> filter (fun p -> (find_int "l4.flags" p) = 18) in
+    join 
+        (fun p -> (rename_keys [("ipv4.src","host")] p,rename_keys [("ipv4.dst","remote")] p))
+        (fun p -> (rename_keys [("ipv4.dst","host")] p,filter_keys ["time"] p))
+        syns synacks
+
+let current_query = completed_flows
 
 let process_file file_name query =
     (of_pcap_file file_name)
